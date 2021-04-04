94b64acebf3b1fe1fd0f367128d4a5f53321dd99
BangMinah-Dev <huytq.act@gmail.com>
2021-04-04 18:29:07 +0700

    update

--- src/App.js
+++ src/App.js
@@ -4,27 +4,22 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import List from "./components/List";
-import ModalCustom from "./components/Modal";
+import ModalCustom from "./components/ModalCustom";
 
 function App() {
     // Hiện thị danh sách
     const [students, setStudentsList] = useState([]);
 
-    // useEffect( () => {
-    //     async function getData(){
-    //         const res = await fetch("https://stdmanagement.herokuapp.com/users")
-    //         const data = await res.json()
-    //         setStudentsList(data)
-    //     }
-    //     getData()
-    // }, [])
-
-    useEffect( () => {
-        async function getData(){
-            console.log("a")
+    useEffect(() => {
+        async function getData() {
+            const res = await fetch(
+                "https://stdmanagement.herokuapp.com/users"
+            );
+            const data = await res.json();
+            setStudentsList(data);
         }
-        getData()
-    },[])
+        getData();
+    }, []);
 
     // Phân biệt modal
     const [whichModal, setWhichModal] = useState("");
@@ -63,27 +58,60 @@ function App() {
     };
 
     // Thêm mới học viên
-    const [newID, setNewID] = useState(12);
+    // const [newID, setNewID] = useState(12);
+
+    // function addNew() {
+    //     setNewID(newID + 1);
+
+    //     let add = {
+    //         id: newID,
+    //         name: studentName,
+    //         birthday: studentBirthDay,
+    //         email: studentEmail,
+    //         phone: studentPhone,
+    //     };
+
+    //     students.unshift(add);
+    //     setShow(false);
+    // }
 
     function addNew() {
-        setNewID(newID + 1);
-
-        let add = {
-            id: newID,
+        let newItem = {
             name: studentName,
             birthday: studentBirthDay,
             email: studentEmail,
             phone: studentPhone,
         };
+        fetch("https://stdmanagement.herokuapp.com/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newItem),
+        });
 
-        students.unshift(add);
+        students.push(newItem);
         setShow(false);
     }
 
     // Chỉnh sửa học viên
+    // function editItem() {
+    //     let newArr = [...students];
+
+    //     let index = newArr.findIndex((student) => student.id === idToRemove);
+    //     if (index > -1) {
+    //         newArr[index].name = studentName;
+    //         newArr[index].birthday = studentBirthDay;
+    //         newArr[index].email = studentEmail;
+    //         newArr[index].phone = studentPhone;
+    //     }
+
+    //     setStudentsList(newArr);
+    //     setShow(false);
+    // }
+
     function editItem() {
         let newArr = [...students];
-
         let index = newArr.findIndex((student) => student.id === idToRemove);
         if (index > -1) {
             newArr[index].name = studentName;
@@ -92,15 +120,48 @@ function App() {
             newArr[index].phone = studentPhone;
         }
 
+        let newItem = {
+            id: idToRemove,
+            name: studentName,
+            birthday: studentBirthDay,
+            email: studentEmail,
+            phone: studentPhone,
+        };
+        fetch(`https://stdmanagement.herokuapp.com/users/${idToRemove}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newItem),
+        });
+
         setStudentsList(newArr);
         setShow(false);
     }
 
     // Xóa học viên
+    // function deleteItem() {
+    //     const removeItem = students.filter(
+    //         (student) => student.id !== idToRemove
+    //     );
+    //     setStudentsList(removeItem);
+    //     setShow(false);
+    // }
+
     function deleteItem() {
         const removeItem = students.filter(
             (student) => student.id !== idToRemove
         );
+        setStudentsList(removeItem);
+
+        fetch(`https://stdmanagement.herokuapp.com/users/${idToRemove}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(),
+        });
+
         setStudentsList(removeItem);
         setShow(false);
     }
